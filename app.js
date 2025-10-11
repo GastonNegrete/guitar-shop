83091066d0c1258b8106958596a4af0110466b23
Gaston Negrete De La Cruz <gaston.negrete@istea.com.ar>
2025-10-11 20:30:27 -0300

    Se crea app.js para empezar a agregar JS al proyecto. Se arma funcion y objeto product para cargar las tarjetas de productos de index desde app.js

---
+++ app.js
@@ -0,0 +1,61 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const listProducts = [
+        {name: 'Fender Vintera II Telecaster', price: 50000, img:'./img/fender-vintera-ii-road-worn-60s-tele-mn-blonde.jpg'},
+        {name: 'Fender Vintera 60 Stratocaster', price: 50000, img:'./img/fender-vintera-ii-road-worn-60s-strat-rw-sbl.jpg'},
+        {name: 'Fender Vintera 50 Jazzmaster', price: 50000, img:'./img/fender-vintera-ii-road-worn-50s-jazzmaster-rw-frd.jpg'},
+        {name: 'Epiphone Les Paul Custom', price: 50000, img:'./img/epiphone-les-paul-custom-quilt-ocean-blue.jpg'},
+        {name: 'Gibson Les Paul Custom 70', price: 50000, img:'./img/gibson-les-paul-custom-70s-eb.jpg'},
+        {name: 'Gretsch Synchromatic Falcon', price: 50000, img:'./img/gretsch-synchromatic-falcon-hollow-sc-bigsby-eb-black.jpg'},
+        {name: 'Yamaha Revstar RSS02T', price: 50000, img:'./img/yamaha-revstar-rss02t-fire-red.jpg'},
+        {name: 'Ibanez JSM10EM-TTB', price: 50000, img:'./img/ibanez-jsm10em-ttb.jpg'}
+    ];
+
+    const productsDomElement = document.querySelector('.productosPrincipal'); //DIV Padre de tarjeta productos
+
+    function createProduct(product){
+        //Creo Tarjeta Producto
+        const newProduct = document.createElement('div');
+        newProduct.setAttribute('class', 'tarjetaProducto');
+
+        const newProductName = document.createElement('h2');
+        newProductName.innerText = product.name;
+    
+        const newImg = document.createElement('img');
+        newImg.setAttribute('src', product.img);
+        newImg.setAttribute('class', 'imgProductosPrincipal');
+        newImg.setAttribute('alt', product.name);
+
+        const newProductPrice = document.createElement('h3');
+        newProductPrice.innerText =  `$ ${product.price}`;
+
+        const newProductButtonDiv = document.createElement('div');
+        newProductButtonDiv.setAttribute('class', 'botonesTarjetaProducto');
+
+        const newCartAnchor = document.createElement('a');
+        newCartAnchor.setAttribute('href', './carrito.html');
+        newCartAnchor.setAttribute('class', 'botonGeneral');
+        newCartAnchor.innerText = 'Agregar al Carrito';
+
+        const newProductAnchor = document.createElement('a');
+        newProductAnchor.setAttribute('href', './producto.html');
+        newProductAnchor.setAttribute('class', 'botonGeneral');
+        newProductAnchor.innerText = 'Ver Producto';
+
+        newProduct.appendChild(newProductName);
+        newProduct.appendChild(newImg);
+        newProduct.appendChild(newProductPrice);
+        newProduct.appendChild(newProductButtonDiv);
+        newProductButtonDiv.appendChild(newCartAnchor);
+        newProductButtonDiv.appendChild(newProductAnchor);
+
+        return newProduct;
+    }
+
+
+    listProducts.forEach(product => {
+        const newProduct = createProduct(product);
+         //Agrego nuevo elemento al DOM
+        productsDomElement.appendChild(newProduct);
+    });
+
+})
