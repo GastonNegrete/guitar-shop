92dcff766819849a9cb95267a317b03eb5f6ac9d
Gaston Negrete De La Cruz <gaston.negrete@istea.com.ar>
2025-10-26 17:45:52 -0300

    Nueva funcion fetch para traer productos desde base de datos de Air Table

--- app.js
+++ app.js
@@ -1,15 +1,9 @@
 document.addEventListener('DOMContentLoaded', () => {
-    //Datos
-    const listProducts = [
-        {name: 'Fender Vintera II Telecaster', price: 50000, img:'./img/fender-vintera-ii-road-worn-60s-tele-mn-blonde.jpg', category: 'guitarra'},
-        {name: 'Fender Vintera 60 Stratocaster', price: 50000, img:'./img/fender-vintera-ii-road-worn-60s-strat-rw-sbl.jpg', category: 'guitarra'},
-        {name: 'Fender Vintera 50 Jazzmaster', price: 50000, img:'./img/fender-vintera-ii-road-worn-50s-jazzmaster-rw-frd.jpg', category: 'guitarra'},
-        {name: 'Epiphone Les Paul Custom', price: 50000, img:'./img/epiphone-les-paul-custom-quilt-ocean-blue.jpg', category: 'guitarra'},
-        {name: 'Gibson Les Paul Custom 70', price: 50000, img:'./img/gibson-les-paul-custom-70s-eb.jpg', category: 'guitarra'},
-        {name: 'Gretsch Synchromatic Falcon', price: 50000, img:'./img/gretsch-synchromatic-falcon-hollow-sc-bigsby-eb-black.jpg', category: 'guitarra'},
-        {name: 'Yamaha Revstar RSS02T', price: 50000, img:'./img/yamaha-revstar-rss02t-fire-red.jpg', category: 'guitarra'},
-        {name: 'Ibanez JSM10EM-TTB', price: 50000, img:'./img/ibanez-jsm10em-ttb.jpg', category: 'amplificador'}
-    ];
+    //Info API
+    const API_TOKEN = 'patvj4vvgsHhiLs1m.c0c21acba723646db966d0a42231149185a5910124f0deea33c633dd362ea3a1';
+    const BASE_ID = 'appBo03PEB9uKUQf3';
+    const TABLE_NAME = 'Products';
+    const API_URL = `https://api.airtable.com/v0/${BASE_ID}/${TABLE_NAME}`;
 
     //Elementos DOM
     const productsDomElement = document.querySelector('.productosPrincipal'); //DIV Padre de tarjeta productos
@@ -82,7 +76,6 @@ document.addEventListener('DOMContentLoaded', () => {
         renderProducts(productsFiltered);
     })
 
-
     categoryFilter.forEach(link =>{
         link.addEventListener('click', (event) => {
             event.preventDefault();
@@ -95,5 +88,42 @@ document.addEventListener('DOMContentLoaded', () => {
             }
     })})
 
-    renderProducts(listProducts);
+
+    //Obtengo productos de AirTable
+    async function getProductsFromAirTable() {
+     
+        try {
+            // Con GET y credenciales obtengo prds de base 
+            const response = await fetch(API_URL, {
+            method: 'GET',
+            headers:{
+                'Authorization': `Bearer ${API_TOKEN}`,
+                'Content-Type': 'application/json' 
+            }
+        });
+
+        //Si se cumple la promesa paso a json los datos recibidos
+        const data = await response.json();
+        console.log('Productos obtenidos OK', data); 
+
+        //Mapeo la data recibida para que coincidan con mis prds
+        const mappedProducts = data.records.map(item => ({
+            name: item.fields.Name,
+            price: item.fields.Price,
+            category: item.fields.Category,
+            img: item.fields.Img
+        }))
+         
+        // Con la funcion que tenia muestro los prods en pantalla
+        renderProducts(mappedProducts);
+
+        } catch (error) {
+            console.log('Error al intentar obtener productos de Air Table.')
+        }
+
+    }
+
+    getProductsFromAirTable();
+
 })
+
